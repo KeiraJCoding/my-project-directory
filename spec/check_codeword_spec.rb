bb2ba458f236e17912f293fe726f1f0c7623b1f8
Keira Jarvis <keira.jarvis@hotmail.co.uk>
2022-10-05 11:03:51 +0100

    added two new test cases

---
+++ spec/check_codeword_spec.rb
@@ -0,0 +1,12 @@
+require "check_codeword.rb"
+
+RSpec.describe "codeword is horse" do
+    it "returns correct if codeword is horse" do
+        result = check_codeword("horse") 
+        expect(result).to eq "Correct! Come in"
+    end
+    it "returns almost if codeword starts with h and ends with e" do
+        result = check_codeword("hearse") 
+        expect(result).to eq "Close but nope"
+    end
+end
